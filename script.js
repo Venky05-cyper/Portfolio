c7d6efca06c7eb3fc0790a3cc7b84962951eccb3
Venky05-cyper <135576529+Venky05-cyper@users.noreply.github.com>
2024-05-24 21:39:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+  window.onscroll = function (){
+      var nav = document.getElementById("nav");
+    var distance = document.documentElement.scrollTop;
+    if(distance >=400){
+      nav.classList.add('scroll');
+    }else{
+      nav.classList.remove('scroll');
+    }
+
+    }
+
+
+    document.querySelector('#but1').addEventListener('click',function () {
+      var name = document.getElementById('inp1').value;
+      var email = document.getElementById('inp2').value;
+    });
 
